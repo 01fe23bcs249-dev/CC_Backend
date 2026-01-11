3db68a6eaf57baf172baa9b0e2f1b50afee534d0
01fe23bcs249-dev <01fe23bcs249@kletech.ac.in>
2026-01-12 02:25:03 +0530

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "/api",
+  baseURL: "https://cloudcomputing-flx3.onrender.com/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
